86d1511aa46524200d7ebf9e9ad9330a17c13f07
SwithinHalee <143343934+SwithinHalee@users.noreply.github.com>
2025-10-06 23:28:21 +0700

    Update : - Added multi select Accounts - Tags fixed

--- lib/screens/manage_accounts_screen.dart
+++ lib/screens/manage_accounts_screen.dart
@@ -13,16 +13,18 @@ class ManageAccountsScreen extends StatefulWidget {
 
 class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
   late List<Account> _accounts;
-
   late List<Account> _filteredAccounts;
   List<String> _allTags = [];
   String? _selectedTag;
+
+  bool _isSelectionMode = false;
+  final Set<Account> _selectedAccounts = {};
 
   @override
   void initState() {
     super.initState();
     _accounts = List<Account>.from(widget.accounts);
-    _filteredAccounts = List<Account>.from(_accounts); // Awalnya tampilkan semua
+    _filteredAccounts = List<Account>.from(_accounts);
     _extractAllTags();
   }
 
@@ -38,16 +40,32 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
 
   void _filterAccounts() {
     setState(() {
-      if (_selectedTag == null) {
-        _filteredAccounts = List<Account>.from(_accounts);
+      _filteredAccounts = _accounts
+          .where((account) =>
+      _selectedTag == null || account.tags.contains(_selectedTag!))
+          .toList();
+    });
+  }
+
+  void _toggleSelection(Account account) {
+    setState(() {
+      if (_selectedAccounts.contains(account)) {
+        _selectedAccounts.remove(account);
       } else {
-        _filteredAccounts = _accounts
-            .where((account) => account.tags.contains(_selectedTag!))
-            .toList();
+        _selectedAccounts.add(account);
+      }
+      if (_selectedAccounts.isEmpty) {
+        _isSelectionMode = false;
       }
     });
   }
-  // --------------------------------------------
+
+  void _clearSelection() {
+    setState(() {
+      _selectedAccounts.clear();
+      _isSelectionMode = false;
+    });
+  }
 
   void _navigateAndAddAccount(BuildContext context) async {
     final newAccount = await Navigator.push<Account>(
@@ -58,14 +76,14 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
     if (newAccount != null) {
       setState(() {
         _accounts.add(newAccount);
+        _extractAllTags();
+        _filterAccounts();
       });
-      _extractAllTags();
-      _filterAccounts();
     }
   }
 
   void _navigateAndEditAccount(BuildContext context, Account account, int index) async {
-    final originalIndex = _accounts.indexWhere((acc) => acc.key == account.key);
+    final originalIndex = _accounts.indexOf(account);
     if (originalIndex == -1) return;
 
     final updatedAccount = await Navigator.push<Account>(
@@ -78,27 +96,28 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
     if (updatedAccount != null) {
       setState(() {
         _accounts[originalIndex] = updatedAccount;
+        _extractAllTags();
+        _filterAccounts();
       });
-      _extractAllTags();
-      _filterAccounts();
     }
   }
 
-  void _deleteAccount(Account accountToDelete) {
+  void _deleteSelectedAccounts() {
     setState(() {
-      _accounts.remove(accountToDelete);
+      _accounts.removeWhere((account) => _selectedAccounts.contains(account));
+      _clearSelection();
     });
     _extractAllTags();
     _filterAccounts();
   }
 
-  void _showDeleteConfirmationDialog(Account account) {
+  void _showMassDeleteConfirmationDialog() {
     showDialog(
       context: context,
       builder: (BuildContext ctx) {
         return AlertDialog(
           title: const Text('Confirm Deletion'),
-          content: const Text('Are you sure you want to delete this account? This action cannot be undone.'),
+          content: Text('Are you sure you want to delete ${_selectedAccounts.length} selected accounts? This action cannot be undone.'),
           actions: <Widget>[
             TextButton(
               child: const Text('Cancel'),
@@ -108,7 +127,7 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
               style: TextButton.styleFrom(foregroundColor: Colors.red),
               child: const Text('Delete'),
               onPressed: () {
-                _deleteAccount(account);
+                _deleteSelectedAccounts();
                 Navigator.of(ctx).pop();
               },
             ),
@@ -118,23 +137,24 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
+        if (_isSelectionMode) {
+          _clearSelection();
+          return false;
+        }
         Navigator.of(context).pop(_accounts);
-        return false;
+        return true;
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Manage Accounts"),
-        ),
+        appBar: _isSelectionMode ? _buildSelectionAppBar() : _buildDefaultAppBar(),
         body: Column(
           children: [
             if (_allTags.isNotEmpty)
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                 child: SizedBox(
                   height: 40,
                   child: ListView.separated(
@@ -163,26 +183,44 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
                 itemCount: _filteredAccounts.length,
                 itemBuilder: (context, index) {
                   final account = _filteredAccounts[index];
+                  final isSelected = _selectedAccounts.contains(account);
                   return ListTile(
-                    leading: CircleAvatar(backgroundColor: account.color),
+                    onLongPress: () {
+                      if (!_isSelectionMode) {
+                        setState(() {
+                          _isSelectionMode = true;
+                          _toggleSelection(account);
+                        });
+                      }
+                    },
+                    onTap: () {
+                      if (_isSelectionMode) {
+                        _toggleSelection(account);
+                      }
+                    },
+                    leading: CircleAvatar(
+                      backgroundColor: isSelected ? Colors.orange : account.color,
+                      child: isSelected ? const Icon(Icons.check, color: Colors.white) : null,
+                    ),
                     title: Text(account.name),
                     subtitle: Text(account.type.name),
-                    trailing: PopupMenuButton<String>(
+                    trailing: _isSelectionMode
+                        ? Checkbox(
+                      value: isSelected,
+                      onChanged: (bool? value) {
+                        _toggleSelection(account);
+                      },
+                    )
+                        : PopupMenuButton<String>(
                       onSelected: (value) {
                         if (value == 'edit') {
                           _navigateAndEditAccount(context, account, index);
-                        } else if (value == 'delete') {
-                          _showDeleteConfirmationDialog(account);
                         }
                       },
-                      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                      itemBuilder: (BuildContext context) => [
                         const PopupMenuItem<String>(
                           value: 'edit',
                           child: Text('Edit'),
-                        ),
-                        const PopupMenuItem<String>(
-                          value: 'delete',
-                          child: Text('Delete'),
                         ),
                       ],
                     ),
@@ -193,10 +231,33 @@ class _ManageAccountsScreenState extends State<ManageAccountsScreen> {
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => _navigateAndAddAccount(context),
+          onPressed: _isSelectionMode ? null : () => _navigateAndAddAccount(context),
+          backgroundColor: _isSelectionMode ? Colors.grey : Colors.orange,
           child: const Icon(Icons.add),
         ),
       ),
+    );
+  }
+
+  AppBar _buildDefaultAppBar() {
+    return AppBar(
+      title: const Text("Manage Accounts"),
+    );
+  }
+
+  AppBar _buildSelectionAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: _clearSelection,
+      ),
+      title: Text('${_selectedAccounts.length} selected'),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.delete),
+          onPressed: _showMassDeleteConfirmationDialog,
+        ),
+      ],
     );
   }
 }
